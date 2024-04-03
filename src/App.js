6deed688c164051e2a78032a726cde312a2f613d
Kelsey Dickson <kelseymedved@gmail.com>
2024-04-03 19:11:25 -0400

    logo added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import logo from "./icons/logo.png";
 function App() {
   return (
     <div className="App">
       <h1>Pottery App</h1>
+      <img src={logo} alt="coconut ceramics logo" />
     </div>
   );
 }
